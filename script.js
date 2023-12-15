4991a199403fcebbe0083eabae8deac297f1ce5b
luisgrbDev <luisgrbenvindo@gmail.com>
2023-12-14 21:54:12 -0300

    criando html e js

---
+++ script.js
@@ -0,0 +1,37 @@
+function enviarTarefas() {
+    const inputTarefa = document.getElementById("novaTarefa");
+    const textoTarefa = inputTarefa.value.trim();
+
+    if(textoTarefa !== ""){
+        const listaTarefas = document.getElementById("listaTarefas");
+        listaTarefas.innerHTML += `
+            <li>
+                <span>${textoTarefa}</span>   
+                <button onclick="marcarConcluida(this)">Concluida</button>
+                <button onclick="removerTarefa(this)">Remover</button>
+            </li>
+        ` 
+
+    }
+}
+
+    function marcarConcluida(botaoConluir){
+
+        const tarefa = botaoConluir.parentNode;
+        //adiciona a classe "completed" para mudar o estilo
+        tarefa.classlist.toggle("completed");
+        if(tarefa.classlist.contains("completed")){
+            //move a tarefa para o final da lista
+            document.getElementById("listaTarefas").appendChild(tarefa);
+
+            //remove os botões das tarefas concluidas
+            const botoes = tarefa.querySelectorAll("button");
+            botoes.forEach(botao => botao.remove());
+            
+        }
+    }
+    
+    function removerTarefa(botaoRemover){
+            botaoRemover.parentNode.remove();
+
+        }
